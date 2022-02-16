5d5808a51c226a7b2dc53bfc2a96ddf396e85c24
Viktor <zeninoid@list.ru>
2022-02-16 17:00:26 +0400

    added one file

---
+++ Lesson3_step6.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+import time
+import math
+
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+
+try:
+    link = "http://suninjuly.github.io/redirect_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    button = browser.find_element_by_css_selector('button.btn').click()
+
+    new_window = browser.window_handles[1]
+    browser.switch_to.window(new_window)
+
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+
+    answer = browser.find_element_by_id("answer")
+    answer.send_keys(y)
+
+    button = browser.find_element_by_css_selector("button.btn").click()
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(5)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
